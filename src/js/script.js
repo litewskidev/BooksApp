6821724e9097242b11d1edb8db786f36e1c9df5d
Adrian Litewski <litewskidev@gmail.com>
2023-01-06 00:41:36 +0100

    determine ratings color & rating bar size

--- src/js/script.js
+++ src/js/script.js
@@ -51,6 +51,9 @@
       const thisBookList = this;
 
       for (let book of thisBookList.data) {
+        book.ratingBgc = thisBookList.determineRatingBgc(book.rating);
+        book.ratingWidth = book.rating * 10;
+
         const generatedHTML = templates.books(book);
         const generatedDOM = utils.createDOMFromHTML(generatedHTML);
         const bookContainer = thisBookList.dom.bookList;
@@ -114,6 +117,22 @@
           hiddenBooks.classList.remove('hidden');
         }
       }
+    }
+
+    determineRatingBgc(rating) {
+      let background = '';
+
+      if (rating < 6) {
+        background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+      } else if (rating > 6 && rating <= 8) {
+        background = 'linear-gradient(to bottom, #b4df5b 0%, #b4df5b 100%)';
+      } else if (rating > 8 && rating <= 9) {
+        background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+      } else if (rating > 9) {
+        background = 'linear-gradient(to bottom, #ff0084 0%, #ff0084 100%)';
+      }
+
+      return background;
     }
   }
 
